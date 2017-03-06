3cb002dbd57b3eb05f3a2243e402e67607570337
Your name here <your email here>
2017-03-06 11:21:24 -0500

    Tweak entry points

--- application/web/app.php
+++ application/web/app.php
@@ -4,13 +4,8 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+//$loader = new ApcClassLoader('ds', $loader);
+//$loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
